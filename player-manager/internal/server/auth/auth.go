8a6b457b06198f841edad7246c642a383b028d6d
Vrangz <krzysztofszulcjr@gmail.com>
2023-02-13 20:29:12 +0100

    add errors

--- player-manager/internal/server/auth/auth.go
+++ player-manager/internal/server/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
 	"player-manager/internal/server/errors"
 	"strings"
@@ -16,12 +17,12 @@ const (
 func SimpleAuthorizationMiddleware(c *gin.Context) {
 	u, p, ok := c.Request.BasicAuth()
 	if !ok || len(strings.TrimSpace(u)) < 1 || len(strings.TrimSpace(p)) < 1 {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, errors.NewError(http.StatusUnauthorized, "empty credentials", nil))
+		c.AbortWithStatusJSON(http.StatusUnauthorized, errors.NewError(http.StatusUnauthorized, "empty credentials", fmt.Errorf("username or password is empty")))
 		return
 	}
 
 	if u != validUsername || p != validPassword {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, errors.NewError(http.StatusUnauthorized, "invalid credentials", nil))
+		c.AbortWithStatusJSON(http.StatusUnauthorized, errors.NewError(http.StatusUnauthorized, "invalid credentials", fmt.Errorf("username or password is invalid")))
 		return
 	}
 }
